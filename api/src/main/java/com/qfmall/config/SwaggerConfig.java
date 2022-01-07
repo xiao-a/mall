1f037ce9da3a4482d0beb6f3d9f432a9876e2949
ji_hua_kai <1261888027@qq.com>
2022-01-07 23:36:30 +0800

    1.api模块整合swagger

---
+++ api/src/main/java/com/qfmall/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.qfmall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger2 配置类
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    /**
+     * 生成接口文档信息
+     * 1.配置生成的文档信息
+     * 2.配置生成规则
+     * Docket 封装文档信息
+     */
+    @Bean
+    public Docket getDocket(){
+
+        // 封面信息对象
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+        apiInfoBuilder.title("《锋迷商城》后端接口文档")
+                .description("接口文档详细信息")
+                .version("2.0.1")
+                .contact(new Contact("姬华凯","www.XXX.com","1261888027@qq.com"));
+        ApiInfo apiInfo = apiInfoBuilder.build();
+
+        // swagger指定文档风格
+        // 指定生成的文档中的封面信息：文档标题、版本、作者
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.qfmall.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+}
--- api/src/main/java/com/qfmall/controller/UserController.java
+++ api/src/main/java/com/qfmall/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.qfmall.controller;
 
+import com.qfmall.fmmall.entity.User;
 import com.qfmall.fmmall.service.UserService;
 import com.qfmall.fmmall.vo.ResultVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -15,8 +17,24 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @RequestMapping("/login")
+    /**
+     * 登录功能
+     * @param userName 用户名
+     * @param password 密码
+     * @return 结果
+     */
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
     public ResultVo checkLogin(String userName,String password){
         return userService.checkLogin(userName,password);
+    }
+
+    /**
+     * 注册功能
+     * @param user 用户信息
+     * @return 结果
+     */
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public ResultVo register(User user){
+        return new ResultVo(10000,"SUCCESS",null);
     }
 }
